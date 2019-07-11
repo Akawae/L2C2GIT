da856a412e57fc237e036da1b3a319d187e0af13
Akawae <larry.georget@gmail.com>
2019-07-11 15:40:37 +0200

    first commit

---
+++ L2C2/templates_origin/index.html.py
@@ -0,0 +1,28 @@
+BBBBBBB BBBBBBBBBBB
+BBBB BBBB
+
+BBBBB BBBBBBBBBB gettext(u'Home') BBBBBBBB
+BBBBB BBBBB gettext(u'Home') BBBBBBBB
+
+BBBBB BBBBBBBBBBBBBBB
+XXX XXXXXXXXXXXXXXX gettext(u'Home') XXXXX
+BBBBBBBB
+
+BBBBB BBBB
+ gettext(u'\n<h2>Congratulations!</h2>\n<p>\n    Welcome to your new Mezzanine powered website.\n    Here are some quick links to get you started:\n</p>\n<ul>\n    <li><a href="/admin/">Log in to the admin interface</a></li>\n    <li><a href="http://mezzanine.jupo.org/docs/content-architecture.html">Creating custom page types</a></li>\n    <li><a href="http://mezzanine.jupo.org/docs/frequently-asked-questions.html#templates">Modifying HTML templates</a></li>\n    <li><a href="http://mezzanine.jupo.org/docs/frequently-asked-questions.html#why-isn-t-the-homepage-a-page-object-i-can-edit-via-the-admin">Changing this homepage</a></li>\n    <li><a href="http://mezzanine.jupo.org/docs/frequently-asked-questions.html">Other frequently asked questions</a></li>\n    <li><a href="http://mezzanine.jupo.org/docs/configuration.html#default-settings">Full list of settings</a></li>\n    <li><a href="http://mezzanine.jupo.org/docs/deployment.html">Deploying to a production server</a></li>\n</ul>\n') 
+SSSSSSSSSSSSSSSSSSSSSSSSS
+SSS
+    SSSSSSS SS SSSS SSS SSSSSSSSS SSSSSSS SSSSSSSS
+    SSSS SSS SSSS SSSSS SSSSS SS SSS SSS SSSSSSSS
+SSSS
+SSSS
+    SSSSSS SSSSSSSSSSSSSSSSSS SS SS SSS SSSSS SSSSSSSSSSSSSSSSSS
+    SSSSSS SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS SSSSSS SSSS SSSSSSSSSSSSSS
+    SSSSSS SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS SSSS SSSSSSSSSSSSSSSSSS
+    SSSSSS SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS SSSS SSSSSSSSSSSSSSSSS
+    SSSSSS SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS SSSSSSSSSS SSSSS SSSSSSSSSSSSSSSSSS
+    SSSSSS SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS SSSS SS SSSSSSSSSSSSSSSSS
+    SSSSSS SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS SS S SSSSSSSSSS SSSSSSSSSSSSSSS
+SSSSS
+
+BBBBBBBB
